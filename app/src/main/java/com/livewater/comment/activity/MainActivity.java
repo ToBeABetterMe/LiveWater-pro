db152e9dab9df2662127e29d43640ab70abde900
Guoquan <happy_aa123@163.com>
2016-08-30 14:06:38 +0800

    增加 TeacherActivity and Chart in it

--- app/src/main/java/com/livewater/comment/activity/MainActivity.java
+++ app/src/main/java/com/livewater/comment/activity/MainActivity.java
@@ -135,7 +135,8 @@ public class MainActivity extends  BaseFragmentActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+             Intent intent=new Intent(MainActivity.this,  TeacherActivity.class);
+             startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
---
+++ app/src/main/java/com/livewater/comment/activity/TeacherActivity.java
@@ -0,0 +1,87 @@
+package com.livewater.comment.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.livewater.comment.R;
+
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.StackedBarChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.StackedBarModel;
+
+public class TeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        BarChart mBarChart = (BarChart) findViewById(R.id.barchart);
+
+        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
+        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
+        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
+
+
+        mBarChart.startAnimation();
+
+        StackedBarChart mStackedBarChart = (StackedBarChart) findViewById(R.id.stackedbarchart);
+
+        StackedBarModel s1 = new StackedBarModel("12.4");
+
+        s1.addBar(new BarModel(2.3f, 0xFF63CBB0));
+        s1.addBar(new BarModel(2.3f, 0xFF56B7F1));
+        s1.addBar(new BarModel(2.3f, 0xFFCDA67F));
+
+        StackedBarModel s2 = new StackedBarModel("13.4");
+        s2.addBar(new BarModel(1.1f, 0xFF63CBB0));
+        s2.addBar(new BarModel(2.7f, 0xFF56B7F1));
+        s2.addBar(new BarModel(0.7f, 0xFFCDA67F));
+
+        StackedBarModel s3 = new StackedBarModel("14.4");
+
+        s3.addBar(new BarModel(2.3f, 0xFF63CBB0));
+        s3.addBar(new BarModel(2.f, 0xFF56B7F1));
+        s3.addBar(new BarModel(3.3f, 0xFFCDA67F));
+
+        StackedBarModel s4 = new StackedBarModel("15.4");
+        s4.addBar(new BarModel(1.f, 0xFF63CBB0));
+        s4.addBar(new BarModel(4.2f, 0xFF56B7F1));
+        s4.addBar(new BarModel(2.1f, 0xFFCDA67F));
+
+        mStackedBarChart.addBar(s1);
+        mStackedBarChart.addBar(s2);
+        mStackedBarChart.addBar(s3);
+        mStackedBarChart.addBar(s4);
+
+        mStackedBarChart.startAnimation();
+
+        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+
+        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
+        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
+
+        mPieChart.startAnimation();
+
+    }
+}
